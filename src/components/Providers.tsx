8c0e012c1e235c4ec0a3e1228ea38ce88b37aab1
mmohsin18 <mmohsin0804@gmail.com>
2024-08-11 11:09:28 +0600

    first commit

---
+++ src/components/Providers.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { PropsWithChildren } from "react"
+import { NextUIProvider } from "@nextui-org/react"
+
+const Providers = ({children}: PropsWithChildren) => {
+  return 
+    <NextUIProvider>
+        {children}
+    </NextUIProvider>
+}
+
+export default Providers
---
+++ src/components/RedirectInput.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+
+const RedirectInput: React.FC = () => {
+  const [url, setUrl] = useState<string>("");
+
+  const handleRedirect = () => {
+    if (url) {
+      const validUrl = url.startsWith("http://") || url.startsWith("https://") ? url : `https://${url}`;
+      window.location.href = "http://localhost:3000/" + validUrl;
+    }
+  };
+
+  return (
+    <div className="w-full text-white flex items-center justify-center px-2">
+      <input
+        type="text"
+        className="border-2 px-2 py-2 rounded-md rounded-r-none  border-zinc-800 w-[60%]"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="Paste your link here"
+      />
+      <button className="w-[5rem] bg-blue-900 px-5 py-2.5 rounded-md rounded-l-none font-bold" onClick={handleRedirect}>Chat</button>
+    </div>
+  );
+};
+
+export default RedirectInput;
